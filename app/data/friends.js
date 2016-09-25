341cd0d60eb27af21a97bcbc84162561f4b05ddd
letocollection <leonard@letocollection.com>
2016-09-25 10:11:17 -0700

    added path

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [{
+    "name": "Leonard Oh",
+    "photo": "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/10534551_10152642926772605_8981196021237457169_n.jpg?oh=107e30b4b8e3bfe5a8c26e1bdab0948d&oe=5875849E",
+    "scores": [
+        5,
+        4,
+        3,
+        3,
+        5,
+        4,
+        3,
+        2,
+        4,
+        3
+    ]
+
+}];
+
+module.exports = friends;
---
+++ app/routing/html-routes.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = function (app) {
+	app.get('/home', function(req, res) {
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	});
+
+	app.get('/reserve', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});	
+
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+};
